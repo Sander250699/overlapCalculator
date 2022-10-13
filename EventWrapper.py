b20ef76f240248943e43c0bc8fda60c4bd654bd4
SanderWipro <sander.maeyer@wipro.com>
2022-10-13 12:47:48 +0200

    added persons

--- EventWrapper.py
+++ EventWrapper.py
@@ -1,8 +1,9 @@
 class Event:
-    def __init__(self, start, end, availability):
+    def __init__(self, start, end, availability, person):
         self.start = start
         self.end = end
         self.availability = availability
+        self.person = person
 
     def print_event(self):
         print(f'Start: {self.start}, end: {self.end}, availability: {self.availability}')
@@ -12,6 +13,9 @@ class Event:
 
     def get_end(self):
         return self.end
+
+    def get_person(self):
+        return self.person.get_name()
 
     def get_availability(self):
         return self.availability
@@ -27,8 +31,10 @@ class AvailabilityEvent:
         self.available_persons = available_persons
 
     def print_event(self):
-        print(f'Start: {self.start}, end: {self.end}')
-        print(self.available_persons)
+        print(f'Start: {self.start}, end: {self.end}, with {self.available_persons}')
+
+    def get_available_persons(self):
+        return self.available_persons
 
     def get_start(self):
         return self.start
--- main.py
+++ main.py
@@ -1,9 +1,27 @@
 from EventWrapper import *
+from PersonsWrapper import *
+from datetime import datetime
 import random
+import argparse
+import csv
 
-limit = 1000
-add_value = 25
-persons = 10
+limit = 100
+add_value = 5
+persons = 2
+
+
+def generate_name():
+    random_string = ''
+
+    for _ in range(5):
+        # Considering only upper and lowercase letters
+        random_integer = random.randint(97, 97 + 26 - 1)
+        flip_bit = random.randint(0, 1)
+        # Convert to lowercase if the flip bit is on
+        random_integer = random_integer - 32 if flip_bit == 1 else random_integer
+        # Keep appending random characters using chr(x)
+        random_string += (chr(random_integer))
+    return random_string
 
 
 def create_events(number_of_persons):
@@ -11,11 +29,13 @@ def create_events(number_of_persons):
     end_times = []
     start_times = []
     all_created = []
+    persons = []
     for x in range(number_of_persons):
         start_times.append(1)
         end_times.append(2)
         event_list.append([])
         all_created.append(False)
+        persons.append(Person(generate_name()))
 
     while all_created.count(True) != len(all_created):
         for x in range(number_of_persons):
@@ -27,83 +47,102 @@ def create_events(number_of_persons):
                     end_time = limit
                     all_created[x] = True
                 end_times[x] = end_time
-                event = Event(start_times[x], end_time, bool(random.getrandbits(1)))
+                event = Event(start_times[x], end_time, bool(random.getrandbits(1)), persons[x])
                 start_times[x] = end_time + 1
                 event_list[x].append(event)
     return event_list
 
 
-def get_event_ends_first_and_available(events, number_of_persons):
+def handle_current_events(events, number_of_persons, next_event):
     global current_event
     current_end = limit + 1
+    available_persons = []
     person = 0
     for x in range(number_of_persons):
+        if next_event is not None and events[x] is not None:
+            events[x].start = next_event.get_end()
         if events[x] is not None and current_end >= events[x].get_end():
             current_event = events[x]
             current_end = events[x].get_end()
             person = x
-    return current_event, person
-
+        if events[x] is not None and events[x].get_availability():
+            available_persons.append(events[x].get_person())
+    return current_event, person, available_persons
 
 def calculate_overlap(events, number_of_persons):
     result_list = []
-    available_persons = []
-    person_list = []
     current_events = [x[0] for x in events]
-
+    next_event = None
     while current_events.count(None) != len(current_events):
-        result = get_event_ends_first_and_available(current_events, number_of_persons)
-        available_persons = []
-        for x in range(number_of_persons):
-            if current_events[x] is not None and current_events[x].get_availability():
-                available_persons.append(x)
+        result = handle_current_events(current_events, number_of_persons, next_event)
+        person_list = events[result[1]]
+        if person_list:
+            person_list.pop(0)
+        if not person_list:
+            # Empty list
+            current_events[result[1]] = None
+        else:
+            current_events[result[1]] = person_list[0]
+            next_event = result[0]
         if len(result_list) >= 1:
             availability_event = AvailabilityEvent(result_list[len(result_list) - 1].get_end(), result[0].get_end(),
-                                                   available_persons)
+                                                   result[2])
         else:
-            availability_event = AvailabilityEvent(result[0].get_start(), result[0].get_end(), available_persons)
+            availability_event = AvailabilityEvent(result[0].get_start(), result[0].get_end(), result[2])
         result_list.append(availability_event)
-        person_list = events[result[1]]
         if result[0].get_end() == limit:
             break
-        else:
-            if person_list:
-                person_list.pop(0)
-            # add new event to list of events
-
-            if not person_list:
-                # Empty list
-                current_events[result[1]] = None
-            else:
-                current_events[result[1]] = person_list[0]
-            for event_to_change in current_events:
-                if event_to_change is not None:
-                    event_to_change.start = result[0].get_end()
     return result_list
 
+
 def get_events(number_of_persons):
+    person1 = Person('Sander')
+    person2 = Person('Emma')
     event_list = []
     list_1 = []
     list_2 = []
-    list_1.append(Event(1, 5, True))
-    list_1.append(Event(5, 7, False))
-    list_1.append(Event(7, 15, False))
-    list_1.append(Event(15, 20, True))
-    list_2.append(Event(1, 2, True))
-    list_2.append(Event(2, 7, True))
-    list_2.append(Event(7, 12, False))
-    list_2.append(Event(12, 20, False))
+    list_1.append(Event(1, 5, True, person1))
+    list_1.append(Event(5, 7, False, person1))
+    list_1.append(Event(7, 15, False, person1))
+    list_1.append(Event(15, 20, True, person1))
+    list_2.append(Event(1, 2, True, person2))
+    list_2.append(Event(2, 7, True, person2))
+    list_2.append(Event(7, 12, False, person2))
+    list_2.append(Event(12, 20, False, person2))
     event_list.append(list_1)
     event_list.append(list_2)
     return event_list
 
 
+def calculate_number_of_slots(events):
+    count = 0
+    for event_list in events:
+        count = count + len(event_list)
+    return count
+
+
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Calculate overlap.')
+    parser.add_argument('persons', metavar='p', default=5, type=int, help='How many persons do you want')
+    parser.add_argument('limit', metavar='l', default=10, type=int, help='end hour (in integer format)')
+    args = parser.parse_args()
+    persons = args.persons
+    limit = args.limit
     event_lists = create_events(persons)
-   # event_lists = get_events(2)
-    #for x in event_lists:
-    #    for y in x:
-    #        y.print_event()
+    # event_lists = get_events(persons)
+    number_slots = calculate_number_of_slots(event_lists)
+    start = datetime.now()
     overlap_list = calculate_overlap(event_lists, persons)
-    for x in overlap_list:
-        x.print_event()
+    end = datetime.now()
+    start_time = start.strftime("%H:%M:%S")
+    end_time = end.strftime("%H:%M:%S")
+    print('Number of persons = ', persons)
+    print('Number of slots= ', number_slots)
+    print("Start time =", start_time)
+    print("End ime =", end_time)
+    fields = ["Start", "End", "Availability"]
+    with open('availability.csv', 'w') as csvfile:
+        filewriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        filewriter.writerow(fields)
+        for x in overlap_list:
+            filewriter.writerow([x.get_start(), x.get_end(), x.get_available_persons()])
